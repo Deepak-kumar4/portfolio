4f1ad0491dd2f5dd545fd2041f67dcc999b4db9a
Deepak Kumar <deepakobc933@gmail.com>
2025-06-26 00:54:07 +0530

    Navbar changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,30 +1,44 @@
 import "./Navbar.css";
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { FiMenu, FiX } from "react-icons/fi";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
   return (
-    <div className="navbar">
+    <nav className="navbar glass-effect">
       <div className="navbar-brand">
-        <Link to="home" smooth={true} duration={600} offset={-60}>Deepak Kumar</Link>
+        <Link to="home" smooth={true} duration={600} offset={-60} onClick={closeMenu}>
+          <span className="logo-text">Deepak Kumar</span>
+        </Link>
       </div>
-      <div className="navbar-links">
-        <button>
-          <Link to="home" smooth={true} duration={600} offset={-60}>Home</Link>
-        </button>
-        <button>
-          <Link to="about" smooth={true} duration={600} offset={-60}>About</Link>
-        </button>
-        <button>
-          <Link to="education" smooth={true} duration={600} offset={-60}>Education</Link>
-        </button>
-        <button>
-          <Link to="projects" smooth={true} duration={600} offset={-60}>Projects</Link>
-        </button>
-        <button>
-          <Link to="contact" smooth={true} duration={600} offset={-60}>Contact</Link>
-        </button>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        {isOpen ? <FiX size={28} /> : <FiMenu size={28} />}
       </div>
-    </div>
+
+      <div className={`navbar-links ${isOpen ? "active" : ""}`}>
+        {["home", "about", "education", "projects", "contact"].map((item) => (
+          <Link
+            key={item}
+            to={item}
+            smooth={true}
+            duration={600}
+            offset={-60}
+            onClick={closeMenu}
+            className="nav-link"
+          >
+            {item.charAt(0).toUpperCase() + item.slice(1)}
+          </Link>
+        ))}
+      </div>
+    </nav>
   );
 }
+
+
 
